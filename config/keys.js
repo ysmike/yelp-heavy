15f99bbae0810b5fc7ced1fe4026621f767b89fa
Mike Lee <xcalibur220@gmail.com>
2019-07-21 21:05:54 -0400

    Add client structure and MongoDB connection

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://<id>:<password>@pley-i09c4.mongodb.net/pley?retryWrites=true&w=majority"
+};
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Restaurants = require("./restaurants");
+
+const app = express();
+app.set("json spaces", 2); // number of spaces for indentation
+
+// DB config using Mongo Atlas
+const uri = require("./config/keys").mongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(uri, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
+
+// @route GET
+app.get("/", (req, res) => {
+  Restaurants.find()
+    .where("reviewCount")
+    .gt(200)
+    .where("price_range")
+    .equals("$$$")
+    .where("cuisine")
+    .nin(["Vegan"])
+    .limit(100)
+    .sort("-percent_high")
+    .select("-_id")
+    .then(docs => res.json(docs))
+    .catch(err => res.status(404).json({ success: false }));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));

04671755e283c9b6cf53033fc831a5d078330a16
Mike Lee <xcalibur220@gmail.com>
2019-08-05 14:46:04 -0400

    Hide credentials

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://mysl:myt22iunVsvvlQBo@pley-i09c4.mongodb.net/pley?retryWrites=true&w=majority"
+    "mongodb+srv://id:pwd@pley-i09c4.mongodb.net/pley?retryWrites=true&w=majority"
 };
